3fe3614d6e11329f56c2416be93e31453ea92eed
Brien Hall <brienhall@Briens-MacBook-Pro.local>
2017-11-17 20:55:03 -0600

    react skeleton

--- src/App.js
+++ src/App.js
@@ -10,9 +10,15 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container">
+          <nav className="navbar">
+            <a className="navbar-brand" href="index.html">KC TRAIL SCOUT (alpha) <i className="fa fa-bicycle" aria-hidden="true"></i></a>
+            <nav className="nav navbar-nav navbar-right" className="text-muted"> 
+              <a id="catalog-link" href="https://brienhall.github.io/catalog/" name="catalog"><span className="text-muted">00004</span></a>
+            </nav>
+          </nav>
+        </div>
+
       </div>
     );
   }
